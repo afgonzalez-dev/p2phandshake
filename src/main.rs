eb0ec117fb96b4392d97934f244411c63e297cf4
FF-agonzalez <agonzalez@forte.io>
2024-06-18 13:20:04 -0300

    Adding Clap for CLI and error handling

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,56 @@
 use std::{str::FromStr, time::Duration};
 
 use alloy_rlp::{Decodable, Encodable};
+use clap::Parser;
 use futures::{SinkExt, StreamExt};
 use reth_ecies::stream::ECIESStream;
 use reth_eth_wire::{HelloMessage, P2PMessage};
 use reth_network_peers::{pk2id, NodeRecord};
 use secp256k1::{rand, SecretKey, SECP256K1};
+use thiserror::Error;
 use tokio::net::TcpStream;
 
+/// Struct for parsing command line arguments
+#[derive(Parser, Debug)]
+#[command(name = "Node Connector")]
+#[command(about = "A CLI for connecting to an Ethereum node", long_about = None)]
+struct Cli {
+    /// NodeRecord string
+    #[arg(long)]
+    node_record: String,
+}
+
+#[derive(Debug, Error)]
+enum CustomError {
+    #[error("Failed to parse node record")]
+    NodeRecordParse,
+    #[error("Failed to extract address and port from node record")]
+    AddressPortParse,
+}
+
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), CustomError> {
+    let cli = Cli::parse();
+    let node_record_str = &cli.node_record;
+    // Split the node_record_str to extract address and port
+    let parts: Vec<&str> = node_record_str.split('@').collect();
+    if parts.len() != 2 {
+        return Err(CustomError::AddressPortParse);
+    }
+
+    let address_port: Vec<&str> = parts[1].split(':').collect();
+    if address_port.len() != 2 {
+        return Err(CustomError::AddressPortParse);
+    }
+
+    let addr = address_port[0];
+    let port: u16 = address_port[1]
+        .parse()
+        .map_err(|_| CustomError::AddressPortParse)?;
+
     let secret_key = SecretKey::new(&mut rand::thread_rng());
-    let outgoing = TcpStream::connect(("86.81.28.194", 30304)).await.unwrap();
-    let node = NodeRecord::from_str("enode://7dcc9ea5437e5ef2fc681292cdb52d9f539ef11cb8404ed1b606bb4aa15199bf461f27e8cfb73d4357a97b3a33e9624b84ad04ddd9cbe136620de38b7c6b3238@86.81.28.194:30304
-    ").unwrap();
+    let outgoing = TcpStream::connect((addr, port)).await.unwrap();
+    let node = NodeRecord::from_str(node_record_str).unwrap();
     let mut client_stream: ECIESStream<TcpStream> =
         ECIESStream::connect(outgoing, secret_key, node.id)
             .await
@@ -37,4 +74,5 @@ async fn main() {
 
     let resp = P2PMessage::decode(&mut &message[..]).unwrap();
     println!("{:?}", resp);
+    Ok(())
 }
